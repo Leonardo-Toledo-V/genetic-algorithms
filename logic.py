5c1a563499d08b4d66d51241d756848b3633ae60
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2024-01-14 00:00:00 -0600

    feat(logic.py): crossover finished first version

--- logic.py
+++ logic.py
@@ -2,6 +2,8 @@ import math
 import random
 from sympy import symbols, lambdify
 
+funcion = "(x**3 * sin(x))/100 + x**2 *cos(x)"
+
 class Individuo:
     def __init__(self, id, binario, i, x, y):
         self.id = id
@@ -9,6 +11,21 @@ class Individuo:
         self.i = i
         self.x = round(x, 4)
         self.y = round(y, 4)
+    def __str__(self):
+        return f"ID: {self.id}, i: {self.i}, Binario: {self.binario}, X: {self.x}, Y: {self.y}"
+
+class Data:
+    num_bits_necesarios = 0
+    rango = 0
+    resolucion = 0
+    limite_inferior = 0
+    limite_superior = 0
+    poblacion_inicial = 0
+    poblacion_maxima = 0
+    tipo_problema_value = ""
+    poblacion_general = []
+    prob_mutacion_ind = 0
+    prob_mutacion_gen = 0
 
 def calcular_funcion(funcion, valor_x):
     x = symbols('x')
@@ -16,82 +33,127 @@ def calcular_funcion(funcion, valor_x):
     resultado = expresion(valor_x)
     return resultado
 
+def calcular_valor_x(limite_inferior, num_generado, resolucion):
+    valor_x = limite_inferior + num_generado*resolucion
+    return valor_x
+
+
 def genetic_algorithm(data):
         
-    poblacion_inicial = int(data.p_inicial)
-    poblacion_maxima = int(data.p_max)
-    resolucion = float(data.res)
-    limite_inferior = float(data.lim_inf)
-    limite_superior = float(data.lim_sup)
-    prob_mutacion_ind = float(data.prob_ind)
-    prob_mutacion_gen = float(data.prob_gen)
-    tipo_problema_value = data.tipo_problema
+    Data.poblacion_inicial = int(data.p_inicial)
+    Data.poblacion_maxima = int(data.p_max)
+    Data.resolucion = float(data.res)
+    Data.limite_inferior = float(data.lim_inf)
+    Data.limite_superior = float(data.lim_sup)
+    Data.prob_mutacion_ind = float(data.prob_ind)
+    Data.prob_mutacion_gen = float(data.prob_gen)
+    Data.tipo_problema_value = data.tipo_problema
     
     #Empezamos sacando los datos 
-    rango = limite_superior - limite_inferior
-    num_saltos = rango/resolucion
+    Data.rango = Data.limite_superior - Data.limite_inferior
+    num_saltos = Data.rango/Data.resolucion
     num_puntos = num_saltos + 1
     num_bits = int(math.log2(num_puntos) + 1)
     rango_numero= 2**num_bits #256
-    num_bits_necesarios = len(bin(rango_numero)[2:])
+    Data.num_bits_necesarios = len(bin(rango_numero)[2:])
     
-    inicializacion(poblacion_inicial, rango_numero, limite_inferior, resolucion, num_bits_necesarios, tipo_problema_value);
+    inicializacion(rango_numero);
 
 
-def inicializacion(poblacion_inicial, rango_numero, limite_inferior, resolucion, num_bits_necesarios, tipo_problema_value):
-    funcion = "(x**3 * sin(x))/100 + x**2 *cos(x)"
-    individuos = []
-    for i in range(poblacion_inicial):
+def inicializacion(rango_numero):
+    for i in range(Data.poblacion_inicial):
         num_generado = (random.randint(0, rango_numero))
-        num_generado_binario = (bin(num_generado)[2:]).zfill(num_bits_necesarios)
-        valor_x = limite_inferior + num_generado*resolucion
+        num_generado_binario = (bin(num_generado)[2:]).zfill(Data.num_bits_necesarios)
+        valor_x = calcular_valor_x(Data.limite_inferior, num_generado, Data.resolucion)
         valor_y = calcular_funcion(funcion, valor_x)
         individuo = Individuo(id=i+1, i=num_generado, binario=num_generado_binario, x=valor_x, y= valor_y)
-        individuos.append(individuo)
+        Data.poblacion_general.append(individuo)
     
-    optimizacion(individuos, tipo_problema_value)
+    optimizacion()
 
 
-def optimizacion(individuos, tipo_problema_value):
+def optimizacion():
     #Estrategia A3: (100 %) la población se ordena y particiona en dos, los individuos de la
     #partición con mejor aptitud se cruzan con algunos o todos los individuos.
     flag = True
-    if tipo_problema_value == "Minimizacion":
+    if Data.tipo_problema_value == "Minimizacion":
         flag = False
-        mejor_individuo = min(individuos, key=lambda x: x.y)
-    else:
-        flag = True
-        mejor_individuo = max(individuos, key=lambda x: x.y)
     # Se ordenan todos los individuos para posteriormente particionarlos
-    individuos_ordenados = sorted(individuos, key=lambda x: x.y, reverse=flag)
+    individuos_ordenados = sorted(Data.poblacion_general, key=lambda x: x.y, reverse=flag)
     
     #Particionamos en dos elementos y separamos la mejor aptitud, y los de peor aptitud
     mitad = int(len(individuos_ordenados) / 2)
     particion_mejor_aptitud = individuos_ordenados[:mitad]
-    otra_parte_poblacion = individuos_ordenados[mitad:]
+    particion_menor_aptitud = individuos_ordenados[mitad:]
 
     for individuo in individuos_ordenados:
         print(f"ID: {individuo.id}, i: {individuo.i}, Binario: {individuo.binario}, X: {individuo.x}, Y: {individuo.y}")
 
-    mejor_valor_y = mejor_individuo.y
-    print(f"ID: {mejor_individuo.id}, Mejor Y: {mejor_valor_y}")
-    print("Poblacion con mejor aptitud", [individuo.id for individuo in particion_mejor_aptitud])
-    print("Resto de poblacion", [individuo.id for individuo in otra_parte_poblacion])
+    print("Poblacion con mejor aptitud:")
+    for individuo in particion_mejor_aptitud:
+        print(individuo)
+    
+    print("Resto de poblacion")
+    resto_poblacion = []
+    for individuo in particion_menor_aptitud:
+        print(individuo)
+        resto_poblacion.append(individuo)
         
+    nueva_poblacion = emparejamiento(resto_poblacion, particion_mejor_aptitud)
+
+
+def emparejamiento(resto_poblacion, particion_mejor_aptitud):
+    nueva_poblacion = []
+    for individuo in resto_poblacion:
+        nueva_poblacion.append(individuo)
+    
+    for individuo in particion_mejor_aptitud:
+        nueva_poblacion.append(individuo)
+
+    for mejor_individuo in particion_mejor_aptitud:
+        for individuo in resto_poblacion:
+            nuevo_individuo1, nuevo_individuo2 = cruza(mejor_individuo, individuo)
+            nueva_poblacion.append(nuevo_individuo1)
+            nueva_poblacion.append(nuevo_individuo2)
+
+    for individuo in nueva_poblacion:
+        print("Nueva poblacion es:", individuo)
+    return nueva_poblacion
 
 
 
 
+def cruza(mejor_individuo, individuo):
+    #Estrategia C1: (90 %) Un punto de cruza aleatorio, para cada pareja a cruzar, de los posibles
+    #puntos de cruza de los individuos se selecciona aleatoriamente la posición.
+    punto_cruza = random.randint(1, Data.num_bits_necesarios -1)
+    print("Punto de cruza", punto_cruza)
+    parte1 = mejor_individuo.binario[:punto_cruza]
+    parte2 = mejor_individuo.binario[punto_cruza:]
+    parte3 = individuo.binario[:punto_cruza]
+    parte4 = individuo.binario[punto_cruza:]
+    
+    nuevo_individuo1 = parte1 + parte4
+    nuevo_individuo2 = parte3 + parte2
+    
+    print("Primer cruce de ", mejor_individuo.id,"a cruzar con:", individuo.id, "=", nuevo_individuo1)
+    print("Segundo cruce de ", mejor_individuo.id,"a cruzar con:", individuo.id, "=", nuevo_individuo2)
+    
+    
+    if((random.randint(1,100))/100 <= Data.prob_mutacion_ind):
+        mutacion(nuevo_individuo1)
+        
+    if((random.randint(1,100))/100 <= Data.prob_mutacion_ind):
+        mutacion(nuevo_individuo2)
+    
+    return nuevo_individuo1, nuevo_individuo2
+    
 
-
-def emparejamiento():
-    print("hello")
-
-def cruza():
-    print("hello")
-
-def mutacion():
-    print("hello")
+def mutacion(individuo):
+    #Estrategia M1: (100 %) Negación del bit
+    #for i in individuo:
+        if((random.randint(1,100))/100 <= Data.prob_mutacion_gen):
+            print("Negación del bit")
 
 def poda():
     print("hello")
